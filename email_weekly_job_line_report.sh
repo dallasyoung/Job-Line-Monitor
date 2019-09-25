28ebe68b4c1846dfb1135a81e1fd052208b0371b
dallasyoung <47343830+dallasyoung@users.noreply.github.com>
2019-09-25 15:28:32 -0600

    Updates mail examples

--- email_weekly_job_line_report.sh
+++ email_weekly_job_line_report.sh
@@ -2,5 +2,7 @@
 
 cat /var/log/jobline/log | mail -s "Job report for $(date)" jhendy@ksp.ca
 cat /var/log/jobline/log | mail -s "Job report for $(date)" dyoung@ksp.ca
+# cat /var/log/jobline/log | mail -s "Job report for $(date)" <your email here>
+# ...
 
 logrotate /var/log/jobline/jobline.logrotate
